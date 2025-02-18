320570dabb69962efec6bb15b6d5f15d3bd8a099
AllisonChacon1207 <62119402+AllisonChacon1207@users.noreply.github.com>
2025-02-18 11:06:35 -0600

    first commit

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+    "name": "Mi Página NFC",
+    "short_name": "MiNFC",
+    "description": "Una aplicación web para gestionar NFC fácilmente."
+
+
+
+    "icons": [
+        {
+            "src": "/imagenes/Logo-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/imagenes/Logo-1024x1024.png",
+            "sizes": "1024x1024",
+            "type": "image/png"
+        }
+    ],
+    "start_url": "/MiPaginaNFC/index.html",
+    "display": "standalone",
+    "background_color": "#F5F5F8",
+    "theme_color": "#F5F5F8",
+    "orientation": "portrait"
+}
